34c48d3f097f3d48532712ecff2e89c139157fd1
Mitchell Hashimoto <mitchellh@mothership.(none)>
2009-03-04 14:50:49 -0800

    Update version based on local

--- lib/lightcloud.rb
+++ lib/lightcloud.rb
@@ -35,7 +35,7 @@ require File.join(File.dirname(__FILE__), 'tyrant_client')
 #  print LightCloud.get("hello") # => nil
 #
 class LightCloud
-  VERSION = '0.1'
+  VERSION = '0.5'
   DEFAULT_SYSTEM = 'default'
 
   @@systems = {}
