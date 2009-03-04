f4e2d692ab78c6c0b5d6ae561a704f4507d0371f
Mitchell Hashimoto <mitchellh@mothership.(none)>
2009-03-04 10:28:46 -0800

    Empty library/spec

---
+++ lib/lightcloud.rb
@@ -0,0 +1,23 @@
+######################################
+# LightCloud
+# Code ported from Python version written by Amir Salihefendic
+######################################
+# Copyright (c) 2009, Mitchell Hashimoto, mitchell.hashimoto@gmail.com
+#
+
+require 'rubygems'
+require 'hash_ring'
+require 'rufus/tokyo'
+
+# = LightCloud Library
+#
+# == Background
+#
+# == Usage
+#
+#  require 'rubygems'
+#  require 'lightcloud'
+#
+class LightCloud
+  VERSION = '0.1'
+end
---
+++ spec/lightcloud_spec.rb
@@ -0,0 +1,4 @@
+require File.join(File.dirname(__FILE__), 'spec_base')
+
+describe LightCloud do
+end
